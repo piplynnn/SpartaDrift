1120fa16e3e55fe0024aef8c66975551d43f566e
realcdub <chwh1021@gmail.com>
2025-02-01 17:10:33 -0500

    horizontal movement with bounds

--- hackathon/Assets/_Scripts/PlayerController.cs
+++ hackathon/Assets/_Scripts/PlayerController.cs
@@ -4,27 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Constants
+    private float MOST_LEFT_LANE = -2;
+     private float MOST_RIGHT_LANE = 2;
+    [SerializeField] private Vector3 displacement = new Vector3(3, 0, 0);
+    [SerializeField] private float speed = 10;
     public Rigidbody2D rb;
     public float rbVelocity = 2f;
-    [SerializeField] private float speed = 20;
-    private Vector3 startingPosition;
     private Vector3 targetPosition;
-    [SerializeField] private Vector3 displacement = new Vector3(3, 0, 0);
+    private Vector3 previousPosition;
+    private float currentLane = 0;
+
+    void Start() {
+        previousPosition = rb.position;
+    }
  
     void Update()
     {
         rb.velocity = Vector3.up * rbVelocity;
 
-        if (Input.GetKeyDown(KeyCode.A)) {
-            startingPosition = rb.position;
-            targetPosition = startingPosition - displacement;             
-        } else if (Input.GetKeyDown(KeyCode.D)) {
-            startingPosition = rb.position;
-            targetPosition = startingPosition + displacement;
+        if (Input.GetKeyDown(KeyCode.A) && currentLane > MOST_LEFT_LANE) {
+            targetPosition = previousPosition - displacement;             
+            previousPosition = targetPosition;
+            currentLane--;
+        } else if (Input.GetKeyDown(KeyCode.D) && currentLane < MOST_RIGHT_LANE) {
+            targetPosition = previousPosition + displacement;
+            previousPosition = targetPosition;
+            currentLane++;
         }
 
-        float rbX = Mathf.Lerp(rb.position.x, targetPosition.x, speed * Time.deltaTime);
-        rb.position = new Vector2(rbX, rb.position.y); // rb.MovePosition(new Vector2(movePosition.x, rb.position.y));
+        float rbSnapX = Mathf.Lerp(rb.position.x, targetPosition.x, speed * Time.deltaTime);
+        rb.position = new Vector2(rbSnapX, rb.position.y);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
